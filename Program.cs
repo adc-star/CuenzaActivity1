07311700cfbb3c5af0763d670e3f0702b012eb1d
adc-star <adriancarlocuenza@gmail.com>
2025-04-06 19:00:43 +0800

    Demo for Fetch

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@ using System;
 
 class Program
 {
-    static int Add(int a, int b, int c)
+    static int Add(int a, int b, int c, int d)
     {
-        return a + b + c;
+        return a + b + c + d;
     }
 
     static void Main()
@@ -18,7 +18,10 @@ class Program
         Console.Write("Enter third number: ");
         int num3 = Convert.ToInt32(Console.ReadLine());
 
-        int result = Add(num1, num2, num3);
+        Console.Write("Enter fourth number: ");
+        int num4 = Convert.ToInt32(Console.ReadLine());
+
+        int result = Add(num1, num2, num3, num4);
         Console.WriteLine("Total: " + result);
     }
 }
