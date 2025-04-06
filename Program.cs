274ad9ff11a2bf632a2af6331a4b6f3b724e86ae
Adrian <Adrian@DESKTOP-V3EH4BI>
2025-04-06 18:46:49 +0800

    Code use to demonstrate commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int result = Add(num1, num2);
+        Console.WriteLine("Total: " + result);
+    }
+}
