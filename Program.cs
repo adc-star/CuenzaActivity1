66026a585b86371563ba8a9e21375ed41ef7d943
Adrian <Adrian@DESKTOP-V3EH4BI>
2025-04-06 19:15:09 +0800

    For separate Branch

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 
 class Program
 {
-    static int Add(int a, int b, int c)
+    static int Subtract(int a, int b, int c)
     {
-        return a + b + c;
+        return a - b - c;
     }
 
     static void Main()
@@ -18,7 +18,7 @@ class Program
         Console.Write("Enter third number: ");
         int num3 = Convert.ToInt32(Console.ReadLine());
 
-        int result = Add(num1, num2, num3);
-        Console.WriteLine("Total: " + result);
+        int result = Subtract(num1, num2, num3);
+        Console.WriteLine("Result: " + result);
     }
 }
