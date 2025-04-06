86c9ee13638ce95d7e50ece2d36a0d780658f221
adc-star <adriancarlocuenza@gmail.com>
2025-04-06 18:57:10 +0800

    Demo for Pulling

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 
 class Program
 {
-    static int Add(int a, int b)
+    static int Add(int a, int b, int c)
     {
-        return a + b;
+        return a + b + c;
     }
 
     static void Main()
@@ -15,7 +15,10 @@ class Program
         Console.Write("Enter second number: ");
         int num2 = Convert.ToInt32(Console.ReadLine());
 
-        int result = Add(num1, num2);
+        Console.Write("Enter third number: ");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+
+        int result = Add(num1, num2, num3);
         Console.WriteLine("Total: " + result);
     }
 }
